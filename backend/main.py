39e5a4f2dddf90d8676e7010806ef96d461bcb6a
boukoulouenoch <boukoulouenoch4@gmail.com>
2026-06-11 14:25:56 +0100

    Features: Création du backend FastAPI avec gérance des erreurs de doublons de numéro de téléphone. Encore en cours de construction

---
+++ backend/main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import psycopg2
+
+
+app = FastAPI()
+
+
+def get_connection():
+    try:
+        connect_db = psycopg2.connect(
+        dbname="KongoPay",
+        user="postgres",
+        password="postgres123",
+        host="localhost",
+        port=5432
+        )
+        print("Connexion réussie!")
+        return connect_db
+
+    except Exception as error:
+     print(f"Erreur de connexion à la base de données: {error}")
+     return None
+
+class inscription(BaseModel):
+    name: str
+    surname: str
+    phone_number: str
+    national_card:str
+    account_type:str
+    password:str
+
+
+@app.post("/inscription")
+def inscription(data: inscription):
+    connection = get_connection()
+    if connection is None:
+        return {"message": "Erreur de connexion"}
+    cursor = connection.cursor()
+    cursor.execute('SELECT*FROM "USERS" WHERE phone_number = %s', (data.phone_number,))
+    result = cursor.fetchone()
+    if result is not None:
+        return{"message":"Ce numéro est déjà utilisé"}
+    cursor.execute(
+        'INSERT INTO "USERS" (name,surname,phone_number,national_card,account_type,password) VALUES (%s,%s,%s,%s,%s,%s)',
+        (data.name,data.surname,data.phone_number,data.national_card,data.account_type,data.password)
+    )
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return {"message": "Compte créé avec succès"}
